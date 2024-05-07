7f86fc701ad171a5959d11e1182f854d88391ec3
Luan Silveira <luancarlos1408@gmail.com>
2024-05-07 09:15:44 -0300

    Começo do projeto
    Comecei ja com a parte do HTML e CSS prontas, agora vou implementando com Javascript

---
+++ app.js
@@ -0,0 +1,8 @@
+function cadastrardespesa(){
+    let ano =document.getElementById ('ano')
+    let mes =document.getElementById ('mes')
+    let dia =document.getElementById ('dia')
+    let tipo =document.getElementById ('tipo')
+    let descricao =document.getElementById ('descricao')
+    let ano =document.getElementById ('valor')
+}
